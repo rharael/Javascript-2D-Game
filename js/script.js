1cfc9a6e38fdca73cddbf79907888487567e867a
rharael <60363332+rharael@users.noreply.github.com>
2022-12-13 02:25:46 -0300

    4 directional player; FPS; and random object

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ window.addEventListener('load', function(){
             this.game = game;
             window.addEventListener('keydown', e=>{
                 this.game.lastKey = 'P' + e.key
-                console.log(this.game.lastKey)
+                // console.log(this.game.lastKey)
             });
             window.addEventListener('keyup', e=>{
                 this.game.lastKey = 'R' + e.key
@@ -22,32 +22,57 @@ window.addEventListener('load', function(){
     class Owlbear {
         constructor(game){
             this.game = game;
-            this.width = 100;
-            this.height = 100;
+            this.spriteWidth = 200;
+            this.spriteHeight = 200;
+            this.width = this.spriteWidth;
+            this.height = this.spriteHeight;
+            this.frameX = 0;
+            this.frameY = 0;
+            this.maxFrame = 30;
             this.x = 200;
             this.y = 200;
             this.speedX = 0;
             this.speedY = 0;
-            this.maxSpeed = 3;
+            this.maxSpeed = 2;
+            this.image = document.getElementById('owlbear');
+            this.fps = 60;
+            this.frameInterval = 1000/this.fps;
+            this.frameTimer = 0;
         }
         draw(context){
-            context.fillRect(this.x, this.y, this.width, this.height);
+            // context.fillRect(this.x, this.y, this.width, this.height);
+            // context.drawImage(this.image, sx, sy, sw, sh, this.x, this.y, this.width, this.height);
+            context.drawImage(this.image, this.frameX * this.spriteWidth, this.frameY * this.spriteHeight, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
         }
         setSpeed(speedX, speedY){
             this.speedX = speedX;
             this.speedY = speedY;
         }
-        update(){
+        update(deltaTime){
             if (this.game.lastKey == 'PArrowLeft'){
                 this.setSpeed(-this.maxSpeed, 0);
+                this.frameY = 3;
+            }else if (this.game.lastKey == 'RArrowLeft' && this.speedX < 0){
+                this.setSpeed(0, 0);
+                this.frameY = 2;
             } else if(this.game.lastKey == 'PArrowRight'){
                 this.setSpeed(this.maxSpeed, 0);
+                this.frameY = 5;
+            } else if(this.game.lastKey == 'RArrowRight' && this.speedX > 0){
+                this.setSpeed(0, 0);
+                this.frameY = 4;
             } else if(this.game.lastKey == 'PArrowUp'){
-                this.setSpeed(0, -this.maxSpeed);
+                this.setSpeed(0, -this.maxSpeed * 0.6);
+                this.frameY = 7;
+            } else if(this.game.lastKey == 'RArrowUp' && this.speedY < 0){
+                this.setSpeed(0, 0);
+                this.frameY = 6;
             } else if(this.game.lastKey == 'PArrowDown'){
-                this.setSpeed(0, this.maxSpeed);
-            } else {
-                this.setSpeed(0,0);
+                this.setSpeed(0, this.maxSpeed * 0.6);
+                this.frameY = 1;
+            } else if(this.game.lastKey == 'RArrowDown' && this.speedY > 0){
+                this.setSpeed(0, 0);
+                this.frameY = 0;
             }
             this.x += this.speedX;
             this.y += this.speedY;
@@ -63,13 +88,71 @@ window.addEventListener('load', function(){
             } else if (this.y > this.game.height - this.height){
                 this.y = this.game.height - this.height
             }
+            // sprite animation
+            if (this.frameTimer > this.frameInterval){
+                this.frameX < this.maxFrame ? this.frameX++ : this.frameX = 0;
+                /*if (this.frameX < this.maxFrame){
+                    this.frameX++;
+                } else {
+                    this.frameX = 0;
+                }*/
+                this.frameTimer = 0;
+            }else {
+                this.frameTimer += deltaTime;
+            }
+
         }
     }
 
     class Object{
-
+        constructor(game){
+            this.game = game;
+        }
+        draw(context){
+            context.drawImage(this.image, this.x, this.y, this.width, this.height);
+        }
     }
+    class Bush extends Object {
+        constructor(game){
+            super(game);
+            this.game = game;
+            this.image = document.getElementById('bush')
+            this.imageWidth = 216;
+            this.imageHeight = 100;
+            this.width = this.imageWidth;
+            this.height = this.imageHeight;
+            this.x = Math.random() * this.game.width - this.width;
+            this.y = this.game.topMargin + Math.random() * (this.game.height - this.height - this.game.topMargin);
 
+
+        }
+    }
+    class Plant extends Object {
+        constructor(game){
+            super(game);
+            this.game = game;
+            this.image = document.getElementById('plant')
+            this.imageWidth = 212;
+            this.imageHeight = 118;
+            this.width = this.imageWidth;
+            this.height = this.imageHeight;
+            this.x = Math.random() * this.game.width - this.width;
+            this.y = this.game.topMargin + Math.random() * (this.game.height - this.height - this.game.topMargin);
+        }
+    }
+    class Grass extends Object {
+        constructor(game){
+            super(game);
+            this.game = game;
+            this.image = document.getElementById('grass')
+            this.imageWidth = 103;
+            this.imageHeight = 183;
+            this.width = this.imageWidth;
+            this.height = this.imageHeight;
+            this.x = Math.random() * this.game.width - this.width;
+            this.y = this.game.topMargin + Math.random() * (this.game.height - this.height - this.game.topMargin);
+        }
+    }
     class Game {
         constructor(width, height){
             this.width = width;
@@ -78,18 +161,35 @@ window.addEventListener('load', function(){
             this.input = new InputHandler(this);
             this.owlbear = new Owlbear(this);
             this.topMargin = 200;
+            this.numberOfPlants = 10;
+            this.plants = [];
         }
-        render(context){
+        render(context, deltaTime){
             this.owlbear.draw(context);
-            this.owlbear.update();
+            this.owlbear.update(deltaTime);
+            this.plants.forEach(plant => plant.draw(context))
+        }
+        init(){
+            
+            for (let i = 0; i < this.numberOfPlants; i++){
+                const randomize = Math.random();
+                if (randomize < 0.3)this.plants.push(new Plant(this));
+                else if  (randomize < 0.6)this.plants.push(new Bush(this));
+                else this.plants.push(new Grass(this));
+            }
         }
     }
 
     const game = new Game(canvas.width, canvas.height);
-    function animante (){
+    game.init();
+    let lastime = 0;
+    function animante (timeStamp){
+        const deltaTime = timeStamp - lastime
+        lastime = timeStamp
+        // console.log(deltaTime)
         ctx.clearRect(0,0,canvas.width, canvas.height);
-        game.render(ctx);
+        game.render(ctx, deltaTime);
         requestAnimationFrame(animante);
     }
-    animante();
+    animante(0);
 });
